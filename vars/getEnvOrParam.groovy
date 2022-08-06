54063c5f7f8956b468da12cb5c3182b5777ca230
Hank Ratzesberger <hr0217@exo.att.com>
2022-08-05 17:21:03 -0700

    GetEnvOrParam.

---
+++ vars/getEnvOrParam.groovy
@@ -0,0 +1,38 @@
+// #!/usr/bin/env groovy - shebang not required.
+
+def call( String name ) {
+
+    def priority = false
+
+    def eVal = env[name]
+    def pVal = params[name]
+
+    // Check that the parameter is specified anywhere.
+    if (eVal == null && pVal == null) {
+        return null
+    }
+
+    // Does environment begin with Bang?
+    eVal = eVal?.trim()
+    if (eVal?.startsWith('!')) {
+        eVal = eVal.substring(1)
+        priority = true
+    }
+
+    // If Environment has priority, then Environment.
+    // Otherwise, if Param is null or false, then Environment.
+    // Otherwise, Parameter.
+    // Boolean may be null, uses short circuit evaluation. 
+    // !pval is Groovy truth applicable to strings.
+    def val = priority ? eVal : ((pVal == null || !pval) ? eVal : pVal)
+    
+    // Boolean parameter types are returned as boolean.
+    switch (pVal?.getClass()?.getName()) {
+        case 'java.lang.Boolean':
+            // Only if there is no matching boolean parameter can it be effectively out scoped.
+            return val.toBoolean()
+        default:
+            return val
+    }
+
+}
